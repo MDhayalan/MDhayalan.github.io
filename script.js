9a5433f21424c7f0e9d894de22aedf6c59d16ee2
Dhayalan <maha.dhaya@gmail.com>
2025-02-10 19:51:28 +0800

    google form script removed

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent default form submission behavior
 
-    // Create a FormData object to hold the form data
-    const formData = new FormData(this);
-
-    // Construct the Google Forms submission URL
-    const googleFormURL = 'https://docs.google.com/forms/d/e/1FAIpQLSeVW4d4cyAXFG_exgz4r9S610RS4XZEB0-9yin01ha5Vx8umQ/formResponse';
-
-
-    // Submit the form data using fetch API
-    fetch(googleFormURL, {
-        method: 'POST',
-        body: formData,
-        mode: 'no-cors', // Required to avoid CORS issues
-    })
-    .then(() => {
-        alert('Form submitted successfully!');
-        this.reset(); // Optionally reset the form
-    })
-    .catch(() => {
-        alert('There was an error submitting the form.');
-    });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll(".carousel-image");
